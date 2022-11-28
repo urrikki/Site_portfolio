1d036298858bbcca8cdf94acb69a5f137d3d86be
Gvbmaster <gnangndong@gaming.tech>
2022-11-28 19:26:36 +0100

    test menu

---
+++ components/menu.php
@@ -0,0 +1,13 @@
+<div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper black">
+        <!-- <a href="#!" class="brand-logo">Logo</a> -->
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#scroll1" class="darken 5 blue-text">Description</a></li>
+          <li><a href="#scroll2" class="darken 5 blue-text">Portfolio</a></li>
+          <li><a href="#scroll3" class="darken 5 blue-text">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
