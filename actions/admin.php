15c9f382af4275b653019f5963abccbba391cfd4
Gvbmaster <gnangndong@gaming.tech>
2022-11-30 08:54:17 +0100

    work on admin

--- actions/admin.php
+++ actions/admin.php
@@ -1,10 +1,8 @@
 <?php
 require_once "../cfg/config.php"; 
-$sql = "UPDATE user SET id=id, admin=";
+$sql = "UPDATE user SET `admin` = '1' WHERE user.`id` = 4";
 $dataBinded=array(
-    ':email'   => $_POST['email'],
-    ':username'   => $_POST['username'],
-    ':password'=> "564#(''#5645616519651654965464'')".$_POST['password'],
+
 );
 $pre = $pdo->prepare($sql);
 $pre->execute($dataBinded);
