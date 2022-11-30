0642e3f03da424a74e3d51a6e2c386323a1a5daf
Gvbmaster <gnangndong@gaming.tech>
2022-11-30 08:36:39 +0100

    fixe sign in

--- actions/signup.php
+++ actions/signup.php
@@ -4,7 +4,7 @@ $sql = "INSERT INTO user(email ,username ,password) VALUES(:email,:username, SHA
 $dataBinded=array(
     ':email'   => $_POST['email'],
     ':username'   => $_POST['username'],
-    ':password'=> $_POST['password'],
+    ':password'=> "564#(''#5645616519651654965464'')".$_POST['password'],
 );
 $pre = $pdo->prepare($sql);
 $pre->execute($dataBinded);
