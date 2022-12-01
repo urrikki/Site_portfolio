4357891ba0f26437a3465802223289a23ba7a7ea
Gvbmaster <gnangndong@gaming.tech>
2022-12-01 13:17:41 +0100

    y a une erreur dans  le mail

--- actions/mail.php
+++ actions/mail.php
@@ -1,12 +1,13 @@
 <?php 
 require_once "../cfg/config.php";
 $email="gnandndong@gaming.tech" && "gtttgttt063@gmail.com";
-$objet=$_POST['object'];
-$content=$_POST['message']."<br><br>Email from : ".$_POST['email'];
+$name=$_POST['name'];
+$objet=$_POST['objet'];
+$content=$_POST['content']."<br><br>Email from : ".$_POST['email'];
 
-if(mail($email, $objet, $content, $header)){
-    echo "Votre message a été envoyé"
+if(mail($email, $objet, $content)){
+    echo "Votre message a été envoyé";
 } else {
-    echo " Une erreur est survenue"
+    echo " Une erreur est survenue";
 }
 ?>
--- index.php
+++ index.php
@@ -133,34 +133,38 @@ if(isset($_SESSION['user'])){
           <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Contactez-nous</a>
         <?php }?>
         <!-- Modal Structure -->
-        <div id="modal1" class="modal">
-          <div class="modal-content">
-            <!--contact-->
-            <!--https://codepen.io/krisantuswanandi/pen/KxrgeZ-->
-            <div class="screen-body">
-              <div class="screen-body-item">
-                <div class="app-form">
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="NOM">
-                  </div>
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="EMAIL">
-                  </div>
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="NUMERO">
-                  </div>
-                  <div class="app-form-group message">
-                    <input class="app-form-control" placeholder="MESSAGE">
-                  </div>
-                  <div class="app-form-group buttons">
-                    <button class="app-form-button">ANNULER</button>
-                    <button class="app-form-button">ENVOYER</button>
+        
+        <form action="actions/mail.php" method="post">
+
+          <div id="modal1" class="modal">
+            <div class="modal-content">
+              <!--contact-->
+              <!--https://codepen.io/krisantuswanandi/pen/KxrgeZ-->
+              <div class="screen-body">
+                <div class="screen-body-item">
+                  <div class="app-form">
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="NOM" type="text" name="name">
+                    </div>
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="EMAIL" type="text" name="email">
+                    </div>
+                    <div class="app-form-group">
+                      <input class="app-form-control" placeholder="Objet" type="text" name="objet">
+                    </div>
+                    <div class="app-form-group message">
+                      <input class="app-form-control" placeholder="MESSAGE" type="textarea" name="content">
+                    </div>
+                    <div class="app-form-group buttons">
+                      <button class="app-form-button">ANNULER</button>
+                      <button class="app-form-button">ENVOYER</button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
       
 
