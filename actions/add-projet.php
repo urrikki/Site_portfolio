e6f2b82c61456eb0c704e46518699858761d463b
Gvbmaster <gnangndong@gaming.tech>
2022-12-01 10:42:13 +0100

    mail

--- actions/add-projet.php
+++ actions/add-projet.php
@@ -1,4 +1,13 @@
 <?php
 require_once "../cfg/config.php"; 
 
-if(isset($_FILES['img']) && !empty($_FILES['img']['name']) && isset($_files['img2']) && !empty($_FILES))
+if(isset($_FILES['img']) && !empty($_FILES['image1']['name']) && isset($_files['image2']) && !empty($_FILES['img2']['name']))
+    $destination1 = "../source/img/".$_FILES['img']['name'];
+    move_uploaded_file($_FILES['img']['tmp_name'],"../".$destination1);
+
+    $destination2 = "../source/img/".$_FILES['img']['name'];
+    move_uploaded_file($_FILES['img']['tmp_name'],"../".$destination2);
+
+    $sql ="INSERT INTO projetpage(h1,img,img2) VALUES(:titre1,:image1
+
+?>
