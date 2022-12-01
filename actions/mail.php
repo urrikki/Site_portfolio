52fbc7999674b241906263281f79a5fb1816161c
Gvbmaster <gnangndong@gaming.tech>
2022-12-01 10:42:21 +0100

    mail

--- actions/mail.php
+++ actions/mail.php
@@ -1,5 +1,12 @@
 <?php 
 require_once "../cfg/config.php";
 $email="gnandndong@gaming.tech" && "gtttgttt063@gmail.com";
-$objet="$_POST"
+$objet=$_POST['object'];
+$content=$_POST['message']."<br><br>Email from : ".$_POST['email'];
+
+if(mail($email, $objet, $content, $header)){
+    echo "Votre message a été envoyé"
+}else {
+    echo " Une erreur est survenue"
+}
 ?>
